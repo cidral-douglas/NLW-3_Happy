207226a2e96c3abab1bdfe81739ae179e8a83ba3
Douglas Cidral <doug.coding20@gmail.com>
2020-10-16 15:26:31 -0300

    Adding the inicial Navigations

--- App-mobile/mobile/src/routes.tsx
+++ App-mobile/mobile/src/routes.tsx
@@ -6,14 +6,21 @@ import { createStackNavigator } from '@react-navigation/stack';
 import OrphanageMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
 
+import SelectMapPositions from './pages/CreateOrphanage/SelectMapPositions';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
+
 const { Navigator, Screen } = createStackNavigator();
 
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
+            <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
                 <Screen name="OrphanageMap" component={OrphanageMap} />
-                <Screen name="OrphanageDetails" component={OrphanageDetails} />
+                <Screen name="OrphanageDetails" component={OrphanageDetails} options={{headerShown: true, header: ()=> <Header showCancel={false} title="Orfanato" />}} />
+                
+                <Screen name="SelectMapaPositions" component={SelectMapPositions} options={{headerShown: true, header: ()=> <Header title="Selecione no mapa" />}} />
+                <Screen name="OrphanageData" component={OrphanageData} options={{headerShown: true, header: ()=> <Header title="Informe os dados" />}} />
             </Navigator>
         </NavigationContainer>
     );
